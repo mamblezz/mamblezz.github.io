a478ac3cfa3a4f61006d38472f4aa541f15bcb3a
mamblezz <mamblezz@gmail.com>
2016-07-19 13:46:13 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,6 @@ function startTime() {
     min = checkTime(min);
     sec = checkTime(sec);
     document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
-
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
-    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
-    var curWeekDay = days[today.getDate()];
-    var curDay = today.getDate();
-    var curMonth = months[today.getMonth()];
-    var curYear = today.getFullYear();
-    var date = curWeekDay + ", " + curDay + " " +curMonth+ " " curYear;
-    document.getElementById("date").innerHTML = date;
     
     var time = setTimeout(function(){startTime() }, 500);
 }
