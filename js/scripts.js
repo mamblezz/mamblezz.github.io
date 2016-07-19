b25b1e077907eb96572838eb7d2663995ccf149d
mamblezz <mamblezz@gmail.com>
2016-07-19 13:42:22 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,28 @@ function startTime() {
     var min = today.getMinutes();
     var sec = today.getSeconds();
     ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
-    hr = (hr === 0) ? 12 : hr;
+    hr = (hr == 0) ? 12 : hr;
     hr = (hr > 12) ? hr - 12 : hr;
     hr = checkTime(hr);
     min = checkTime(min);
     sec = checkTime(sec);
-    
     document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
-    
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    var curWeekDay = days[today.getDay()];
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
+    var curWeekDay = days[today.getDate()];
     var curDay = today.getDate();
     var curMonth = months[today.getMonth()];
     var curYear = today.getFullYear();
-    var date = curWeekDay + ", " + curDay + " " + curMonth + " " + curYear;
+    var date = curWeekDay + ", " + curDay + " " +curMonth+ " " curYear;
     document.getElementById("date").innerHTML = date;
     
-    var time = setTimeout(function(){startTime()}, 500);
+    var time = setTimeout(function(){startTime() }, 500);
 }
 
 function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
-        }
-    return i;
+if (i < 10) {
+    i = "0" + 1;
     }
+return i;
 }
