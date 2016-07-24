dcc51fb0bd3c0d2ada61332d7ca7f063e31ff37e
Marble T. Resurreccion <mamblezz@gmail.com>
2016-07-25 00:26:32 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,3 @@
-var main = function() {
-    $('.menu').click(function() {
-        $('.menu').animate({
-        left: "-285px"
-        }, 200);
-        $('body').animate({
-            left: "0px"
-        }, 200);
-    });
-    $('.icon-menu').click(function(){
-        $('.menu').animate({
-            left: "0px"
-        }, 200);
-        $('body').animate({
-            left: "285px"
-        }, 200);
-    });
-};
-
-$(document).ready(main);
-
 window.onclick = myFunction;
     function myFunction() {
         document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
