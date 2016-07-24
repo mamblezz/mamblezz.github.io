6cc893e886419c38b5dfb6db97c2bd43eb3e83b1
Marble T. Resurreccion <mamblezz@gmail.com>
2016-07-24 22:53:08 +0800

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+var main = function() {
+    $('.menu').click(function() {
+        $('.menu').animate({
+        left: "-285px"
+        }, 200);
+        $('body').animate({
+            left: "0px"
+        }, 200);
+    });
+    $('.icon-menu').click(function(){
+        $('.menu').animate({
+            left: "0px"
+        }, 200);
+        $('body').animate({
+            left: "285px"
+        }, 200);
+    });
+};
+
+$(document).ready(main);
